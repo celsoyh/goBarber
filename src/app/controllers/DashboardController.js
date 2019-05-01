bea543956611aa47b7b3e7cb35003131472bedbf
Celso Hashimoto <celsoyh@gmail.com>
2019-05-01 20:41:46 -0300

    Provider can now check his scheduled clients

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,9 +1,29 @@
-const { User } = require('../models');
+const { User, Appointment } = require('../models');
+const moment = require('moment');
+moment.locale('pt-br');
 
 class DashboardController {
   async index(req, res) {
     const providers = await User.findAll({ where: { provider: true } });
-    return res.render('app/dashboard', { providers });
+    let appointments = await Appointment.findAll({
+      where: { provider_id: req.session.user.id }
+    });
+
+    Promise.all(
+      appointments.map(async a => {
+        let user = await User.findOne({ where: { id: a.user_id } });
+        let newdate = moment(parseInt(a.date.getTime())).format('LLLL');
+        console.log('newdate', newdate);
+        return {
+          user_id: a.id,
+          date: newdate,
+          name: user.name,
+          avatar: user.avatar
+        };
+      })
+    ).then(appUsers => {
+      return res.render('app/dashboard', { providers, appUsers });
+    });
   }
 }
 
